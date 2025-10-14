da12d2ed12c499a41c3f88aba812d009c0fa1856
robjor88 <robjor88@gmail.com>
2025-10-14 17:33:42 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import express from "express"
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.static("public"))
+
+
+app.listen(PORT, () => {
+  console.log(`server runs on port: ${PORT}`)
+})
+
+
+
